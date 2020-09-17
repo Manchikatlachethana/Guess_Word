8e7e6912ac5b3d74bdf253ef79afc2e733ede88e
chethana <62558574+Manchikatlachethana@users.noreply.github.com>
2020-09-17 21:51:17 +0530

    word_guess v1.1

--- word_guess.py
+++ word_guess.py
@@ -1,11 +1,14 @@
 import time,random
 name=input('what is your name? ')
-print("Hello,",name,"Time to play Hangman!")
+time.sleep(1)
+print("Hello,",name,"Time to play!")
+time.sleep(1)
+print("Let the game begin..")
 time.sleep(1)
 print("Good Luck !", name)
 time.sleep(1)
 words=["secret",'game','python','java','technology','computer','science','android','apple','keyboard']
-print("sart guessing....!")
+print("start guessing....!")
 time.sleep(0.5)
 word=random.choice(words)
 guesses=''
@@ -14,18 +17,20 @@ while turns>0:
 	failed=0
 	for char in word:
 		if char in guesses:
-			print(char)
+			print(char, end=' ')
 		else:
-			print('-')
+			print('-',end=' ')
 			failed+=1
 	if failed==0:
-		print(name,"won!")
+		print("\n ",name,"won!")
+		print("word is ",word)
 		break
-	guess=input("guess the letter...")
+	guess=input("\nguess the letter... : ")
 	guesses+=guess
 	if guess not in word:
 		turns-=1
 		print("wrong")
 		print("you have",turns,"more chances!")
 		if turns==0:
-		   print(name,"lost!")			
+		   print(name,"lost!")
+		   print("word is : ",word)			
