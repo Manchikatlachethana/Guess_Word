24529bae701a2208c97ae6549ba399a64e86b845
Ujjawal179 <72138726+Ujjawal179@users.noreply.github.com>
2020-10-04 08:44:11 +0530

    Code and Docs improved

--- word_guess.py
+++ word_guess.py
@@ -1,5 +1,5 @@
 import time,random
-name=input('what is your name? ')
+name=input('what is your name?:\n ')
 time.sleep(1)
 print("Hello,",name,"Time to play!")
 time.sleep(1)
@@ -12,7 +12,7 @@ print("start guessing....!")
 time.sleep(0.5)
 word=random.choice(words)
 guesses=''
-turns=12
+turns=10
 while turns>0:
 	failed=0
 	for char in word:
@@ -22,15 +22,13 @@ while turns>0:
 			print('-',end=' ')
 			failed+=1
 	if failed==0:
-		print("\n ",name,"won!")
+		print("\n ","Great your guess is right \n Wow, you won ;-)\n",name,"won!")
 		print("word is ",word)
 		break
 	guess=input("\nguess the letter... : ")
 	guesses+=guess
 	if guess not in word:
 		turns-=1
-		print("wrong")
-		print("you have",turns,"more chances!")
+		print(f"Oh no, you was too close \n Try again you have {turns} more chances to guess")
 		if turns==0:
-		   print(name,"lost!")
-		   print("word is : ",word)			
+		   print(f"Better luck next time and answer was {word}. \n GAME OVER \n Hope you will come again")			
